89f0653b6d4884f4c20c40ee75c851ca589adc5b
Gretten <shhworkmail@yandex.ru>
2021-01-30 10:11:29 +0300

    dotenv crap

--- api/api.js
+++ api/api.js
@@ -1,8 +1,6 @@
 const fetch = require('node-fetch');
 
-const city = 'Moscow';
 const APIKey = 'a0408c4d5fded32e057194a7540e3f41';
-
 
 const getServiceResponse = async (city, key) => {
     const APILink = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`;
@@ -17,17 +15,18 @@ const getServiceResponse = async (city, key) => {
     }
 }
 
-// todo: create a reference between the first function and the second
+const dataHandler = ( city ) => {
+    const json = getServiceResponse(city, APIKey);
+    return json.then(res => {
+        const result = Object.assign({}, ...function _flatten(o) { return [].concat(...Object.keys(o).map(k => typeof o[k] === 'object' ? _flatten(o[k]) : ({[k]: o[k]})))}(res))
 
-const dataHandler = ( json ) => {
-    const target = JSON.parse(json);
-
-    const res = Object.assign({}, ...function _flatten(o) { return [].concat(...Object.keys(o).map(k => typeof o[k] === 'object' ? _flatten(o[k]) : ({[k]: o[k]})))}(target))
-
-    const { main, icon, temp, feels_like, temp_min, temp_max, pressure, humidity, visibility, speed } = res;
-
-    return {
-        mainWeather: { main, icon, temp, feels_like, temp_min, temp_max },
-        details: { pressure, humidity, visibility, speed }
-    }
+        const { main, icon, temp, feels_like, temp_min, temp_max, pressure, humidity, visibility, speed } = result;
+    
+        return {
+            mainWeather: { main, icon, temp, feels_like, temp_min, temp_max },
+            details: { pressure, humidity, visibility, speed }
+        }
+    })
 }
+
+export default dataHandler;
