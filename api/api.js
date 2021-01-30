e432560e6da6889201d662c3ba2c3e5c20942c25
Gretten <shhworkmail@yandex.ru>
2021-01-30 10:32:15 +0300

    dotenv setup successful

--- api/api.js
+++ api/api.js
@@ -1,6 +1,8 @@
+require('dotenv').config({path: '../.env'});
 const fetch = require('node-fetch');
 
-const APIKey = 'a0408c4d5fded32e057194a7540e3f41';
+
+const APIKey = process.env.API_KEY;
 
 const getServiceResponse = async (city, key) => {
     const APILink = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`;
@@ -18,15 +20,20 @@ const getServiceResponse = async (city, key) => {
 const dataHandler = ( city ) => {
     const json = getServiceResponse(city, APIKey);
     return json.then(res => {
+        // some magic from SO
         const result = Object.assign({}, ...function _flatten(o) { return [].concat(...Object.keys(o).map(k => typeof o[k] === 'object' ? _flatten(o[k]) : ({[k]: o[k]})))}(res))
 
         const { main, icon, temp, feels_like, temp_min, temp_max, pressure, humidity, visibility, speed } = result;
     
-        return {
-            mainWeather: { main, icon, temp, feels_like, temp_min, temp_max },
-            details: { pressure, humidity, visibility, speed }
-        }
+        return console.log( {
+            mainWeather: { 
+                main, icon, temp, feels_like, temp_min, temp_max 
+            },
+            details: { 
+                pressure, humidity, visibility, speed 
+            },
+        })
     })
 }
 
-dataHandler('Moscow');
+export default dataHandler;
