9731e071d210567a754cdecdb9473a3a75aec049
Gretten <shhworkmail@yandex.ru>
2021-02-14 15:26:18 +0300

    the weatherObjectBuilder method was created

--- src/api/api.js
+++ src/api/api.js
@@ -10,7 +10,50 @@ class OpenWeatherApi {
         this.response = null;
     }
 
-    #getApiUrl = () => `${this.baseLink}?q=${this.city}&units=${this.units}&appid=${this.apiKey}`;
+    getApiUrl = () => `${this.baseLink}?q=${this.city}&units=${this.units}&appid=${this.apiKey}`;
+
+    weatherObjectBuilder(response) {
+        let data = response.data;
+        let main = data.main;
+
+        return {
+            mainWeather: {
+                city: data.name,
+                temp: Math.round(main.temp),
+                minTemp: Math.round(main.temp_min),
+                maxTemp: Math.round(main.temp_max),
+                feelsLike: main.feels_like,
+                currentState: data.weather[0].main,
+                icon: data.weather[0].icon,
+            },
+            details: [
+                {
+                    payload: main.pressure,
+                    title: 'Pressure',
+                    icon: 'fas fa-water',
+                    measure: 'Pa',
+                },
+                {
+                    payload: main.humidity,
+                    title: 'Humidity',
+                    icon: 'fas fa-temperature-high',
+                    measure: '%',
+                },
+                {
+                    payload: data.visibility,
+                    title: 'Visibility',
+                    icon: 'fas fa-cloud',
+                    measure: 'm',
+                },
+                {
+                    payload: data.wind.speed,
+                    title: 'Speed',
+                    icon: 'fas fa-wind',
+                    measure: 'm/s',
+                },
+            ]
+        }
+    }
 
     setCity(city) {
         this.city = city;
@@ -24,45 +67,7 @@ class OpenWeatherApi {
         this.retrieve();
         return this.response
             .then(res => {
-                let data = res.data;
-                let main = data.main;
-                
-                return {
-                    mainWeather: {
-                        city: data.name,
-                        temp: Math.round(main.temp),
-                        minTemp: Math.round(main.temp_min),
-                        maxTemp: Math.round(main.temp_max),
-                        feelsLike: main.feels_like,
-                        currentState: data.weather[0].main,
-                        icon: data.weather[0].icon,
-                    },
-                    details: [
-                        {
-                            payload: main.pressure,
-                            title: 'Pressure',
-                            icon: 'fas fa-water',
-                            measure: 'Pa',
-                        },
-                        {
-                            payload: main.humidity,
-                            title: 'Humidity',
-                            icon: 'fas fa-temperature-high',
-                            measure: '%',
-                        },
-                        {
-                            payload: data.visibility,
-                            title: 'Visibility',
-                            icon: 'fas fa-cloud',
-                            measure: 'm',
-                        },
-                        {
-                            payload: data.wind.speed,
-                            title: 'Speed',
-                            icon: 'fas fa-wind',
-                            measure: 'm/s',
-                        },
-                    ]}
+                return this.weatherObjectBuilder(res)
             })
             .catch((err) => {
                 return console.log(err)
@@ -70,7 +75,7 @@ class OpenWeatherApi {
     }
 
     retrieve() {
-        this.response = axios.get(this.#getApiUrl())
+        this.response = axios.get(this.getApiUrl())
     }
 }
 
