7c82546853ae95f5a910a66950f754603545c0b9
Gretten <shhworkmail@yandex.ru>
2021-02-14 16:36:03 +0300

    codestyle improvements

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+import objectBuilder from '@/assets/helpers.js'
 const APIKey = process.env.VUE_APP_API_KEY;
 
 class OpenWeatherApi {
@@ -7,8 +8,9 @@ class OpenWeatherApi {
         this.units = 'metric';
         this.city = '';
         this.response = null;
-        this.baselink = 'https://api.openweathermap.org/data/2.5/weather';
     }
+
+    baseLink = 'https://api.openweathermap.org/data/2.5/weather';
 
     setCity(city) {
         this.city = city;
@@ -17,75 +19,18 @@ class OpenWeatherApi {
     setUnits(units) {
         this.units = units;
     }
-    
-    getApiUrl() {
-        return `${this.baselink}?q=${this.city}&units=${this.units}&appid=${this.apiKey}`;
-    } 
 
-    objectBuilder(response) {
-        return response;
+    getApiUrl() {
+        return `${this.baseLink}?q=${this.city}&units=${this.units}&appid=${this.apiKey}`;
     }
 
     getWeather() {
         return axios.get(this.getApiUrl())
-            .then(res => {
-                return this.objectBuilder(res)
-            })
-            .catch((err) => {
-                return console.log(err)
-        })
+            .then(objectBuilder)
+            .catch(console.error)
     }
 }
 
-class OpenWeatherBuilder extends OpenWeatherApi {
-    constructor(apiKey) {
-        super(apiKey)
-    }
-
-    objectBuilder(response) {
-        let data = response.data;
-        let main = data.main;
-
-        return {
-            mainWeather: {
-                city: data.name,
-                temp: Math.round(main.temp),
-                minTemp: Math.round(main.temp_min),
-                maxTemp: Math.round(main.temp_max),
-                feelsLike: main.feels_like,
-                currentState: data.weather[0].main,
-                icon: data.weather[0].icon,
-            },
-            details: [
-                {
-                    payload: main.pressure,
-                    title: 'Pressure',
-                    icon: 'fas fa-water',
-                    measure: 'Pa',
-                },
-                {
-                    payload: main.humidity,
-                    title: 'Humidity',
-                    icon: 'fas fa-temperature-high',
-                    measure: '%',
-                },
-                {
-                    payload: data.visibility,
-                    title: 'Visibility',
-                    icon: 'fas fa-cloud',
-                    measure: 'm',
-                },
-                {
-                    payload: data.wind.speed,
-                    title: 'Speed',
-                    icon: 'fas fa-wind',
-                    measure: 'm/s',
-                },
-            ]
-        }
-    }
-}
-
-const apiInstance = new OpenWeatherBuilder(APIKey);
+const apiInstance = new OpenWeatherApi(APIKey);
 
 export default apiInstance;
---
+++ src/assets/helpers.js
@@ -0,0 +1,43 @@
+const objectBuilder = response => {
+    let data = response.data;
+    let main = data.main;
+
+    return {
+        mainWeather: {
+            city: data.name,
+            temp: Math.round(main.temp),
+            minTemp: Math.round(main.temp_min),
+            maxTemp: Math.round(main.temp_max),
+            feelsLike: main.feels_like,
+            currentState: data.weather[0].main,
+            icon: data.weather[0].icon,
+        },
+        details: [{
+                payload: main.pressure,
+                title: 'Pressure',
+                icon: 'fas fa-water',
+                measure: 'Pa',
+            },
+            {
+                payload: main.humidity,
+                title: 'Humidity',
+                icon: 'fas fa-temperature-high',
+                measure: '%',
+            },
+            {
+                payload: data.visibility,
+                title: 'Visibility',
+                icon: 'fas fa-cloud',
+                measure: 'm',
+            },
+            {
+                payload: data.wind.speed,
+                title: 'Speed',
+                icon: 'fas fa-wind',
+                measure: 'm/s',
+            },
+        ]
+    }
+}
+
+export default objectBuilder;
